1db2f54b1895f9f5af7bd6a0ea5d796953cc7340
yongma16 <17685043634@163.com>
2020-09-02 15:48:28 +0800

    2020-09-02C++学习日记
    类的前向声明、构造函数和析构函数（birth and die）、内联函数、左值是存在地址空间（fun()=variable）、下一步学习友元类和友元函数（github的中文乱码现象）

--- C++_project/Study_day02/Study_day02/2020-09-02.cpp
+++ C++_project/Study_day02/Study_day02/2020-09-02.cpp
@@ -6,22 +6,42 @@ using std::cout;
 using std::cin;
 using std::endl;
 using std::string;
-//多继承的练习和友元的使用
-//assert预处理宏
+//内联函数和赋值，拷贝，析构函数，返回*this的成员函数
 class Screen {
 public:
-	typedef string::size_type pos;//移动光标和读取位置，无符号类型
-	Screen() = default;//默认无参的构造函数
-	Screen(pos ht, pos wd, char c) : height(ht), width(wd), contents(ht* wd, c) {}//
+	typedef string::size_type pos;//声明pos类型，移动光标和读取位置，无符号类型
+	Screen() {
+		cout << "Screen被创建了!"<<endl;
+	};//无参的构造函数
+	Screen(pos ht, pos wd, char c) : height(ht), width(wd), contents(ht* wd, c) {}//有参构造函数
 	//等价height=ht;width=wd;contents=c;
-
+	Screen &move(pos r,pos c);//在类的外部之后设置为内联函数
+	char get() const {
+		return contents[cursor];//隐式内联
+	}
+	inline char get(pos ht, pos wd) const;//显示内联
+	~Screen() {
+		cout << "Screen被销毁了" << endl;
+	}
 private:
 	pos cursor = 0, height = 0, width = 0;
 	string contents;
 };
+inline Screen &Screen::move(pos r,pos c)//内联函数
+{
+	pos row = r * width;
+	cursor = row + c;
+	return *this;//左值的方式返回对象，个人理解为有内存空间地址
+}
+char Screen::get(pos r, pos c) const
+{
+	pos row = r * width;//行的位置
+	return contents[row + c];//列的字符
+}
 int main(int argc, char* argv)
 {
-	Screen s;
-	
+	Screen s;//声明Screen类
+	char ch = s.get();
+	ch = s.get(0, 0);
 	return 0;
 }
