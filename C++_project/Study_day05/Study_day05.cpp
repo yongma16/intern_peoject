77bd6231137223b89b15282a1bf73b57332e5214
yongma16 <17685043634@163.com>
2020-09-05 15:23:28 +0800

    账号修改成功

---
+++ C++_project/Study_day05/Study_day05.cpp
@@ -0,0 +1,59 @@
+﻿#include <iostream>
+#include<string>
+using namespace std;
+//学习多继承和抽象数据类型，依赖接口和实现分离编程
+
+class Intern
+{
+public:
+    string company = "gamescube";
+    virtual void Intern_interface();
+    Intern()
+    {
+        cout << "Intern has been created!\n";
+    }
+    ~Intern()
+    {
+        cout << "Inter has been destroyed!\n";
+    }
+private:
+    string things = "just beat it!";
+    string reason = "my heart is broken！";
+    
+};
+
+class People
+{
+public:
+    string name = "fool";
+    People()
+    {
+        cout << "People has been created!\n";
+    }
+    ~People()
+    {
+        cout << "People has been destroyed!\n";
+    }
+private:
+    string number = "20200905";
+};
+
+
+class Human:Intern,People{//多继承
+public:
+    void People::Intern_function() {
+
+    }
+    string name = "Human";
+    
+private:
+    string password = "123456789";
+};
+
+
+int main(int argc,char *argv)
+{
+    cout << "Hello World!\n";
+    return 0;
+}
+
